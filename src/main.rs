3aef3f758b4e9fe32580bc5dee9f128b9d67aeb5
kevetka <redpatrik2007@gmail.com>
2025-10-27 00:06:19 +0300

    Adding cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -7,26 +7,23 @@ fn main() {
         let mut input = String::new();
 
         match io::stdin().read_line(&mut input) {
-            Ok(_) => {
-                match input.trim().parse::<i32>() {
-                    Ok(num) => {
-                        if num == -1 {
-                            break;
-                        }
-                        if num > 0 {
-                            summa += num;
-                        }
-                        else {
-                            println!("NaN");
-                            return;
-                        }
+            Ok(_) => match input.trim().parse::<i32>() {
+                Ok(num) => {
+                    if num == -1 {
+                        break;
                     }
-                    Err(_) => {
+                    if num > 0 {
+                        summa += num;
+                    } else {
                         println!("NaN");
                         return;
                     }
                 }
-            }
+                Err(_) => {
+                    println!("NaN");
+                    return;
+                }
+            },
             Err(_) => {
                 println!("Nan");
                 return;
