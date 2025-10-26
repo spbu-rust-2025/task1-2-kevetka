1dc89dbb0cb292dcddfc28903406034a7d73c591
kevetka <redpatrik2007@gmail.com>
2025-10-27 00:01:28 +0300

    Adding rust code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
+use std::io;
 
+fn main() {
+    let mut summa = 0;
+
+    loop {
+        let mut input = String::new();
+
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {
+                match input.trim().parse::<i32>() {
+                    Ok(num) => {
+                        if num == -1 {
+                            break;
+                        }
+                        if num > 0 {
+                            summa += num;
+                        }
+                        else {
+                            println!("NaN");
+                            return;
+                        }
+                    }
+                    Err(_) => {
+                        println!("NaN");
+                        return;
+                    }
+                }
+            }
+            Err(_) => {
+                println!("Nan");
+                return;
+            }
+        }
+    }
+    println!("{}", summa);
 }
